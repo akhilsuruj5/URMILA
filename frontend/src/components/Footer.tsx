68b338d93737a027e1451a2bcbc2ebcd4e9b2bf1
bllamond <vinaymalik1729@gmail.com>
2024-10-04 00:10:31 +0530

    Setup Backend

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-gray-200 h-44'>Footer</div>
+  )
+}
+
+export default Footer
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Pahuche</div>
+    
+  )
+}
+
+export default Home
